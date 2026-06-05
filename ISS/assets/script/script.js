ad452bf920aad1079a2e3e7b0bb54259e9de1312
Thiago Neri <thiagonerieu@gmail.com>
2026-06-05 15:24:07 +0200

    Correction de mon fichier d'evaluation et ajout du fichier de correction d'Yoann.

--- ISS/assets/script/script.js
+++ ISS/assets/script/script.js
@@ -1,93 +1,118 @@
 /***************
   MAP LEAFLET
 ***************/
-//EXO 1.1
-const map = L.map('map').setView([0, 0], 5); 
+//Map Mayer Creation
+const MAP = L.map('map').setView([0, 0], 5); 
 
-// EXO 1.2
+//Add Tile Layer to Map
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+}).addTo(MAP);
 
-let marker = L.marker([0, 0]).addTo(map);
-
-let circle = L.circle([51.508, -0.11], {
-    color: 'red',
-    fillColor: '#f03',
-    fillOpacity: 0.5,
-    radius: 500
-}).addTo(map);
-
-let polygon = L.polygon([
-    [51.509, -0.08],
-    [51.503, -0.06],
-    [51.51, -0.047]
-]).addTo(map);
-
-marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
-circle.bindPopup("I am a circle.");
-polygon.bindPopup("I am a polygon.");
-
-// let popup = L.popup()
-//     .setLatLng([51.513, -0.09])
-//     .setContent("I am a standalone popup.")
-//     .openOn(map);
-
-// function onMapClick(e) {
-//     alert("You clicked the map at " + e.latlng);
-// }
-
-map.on('click', onMapClick);
-
-let popup = L.popup();
-
-function onMapClick(e) {
-    popup
-        .setLatLng(e.latlng)
-        .setContent("You clicked the map at " + e.latlng.toString())
-        .openOn(map);
-}
-
-map.on('click', onMapClick);
-
-// EXO 1.3
+//Creation of ISS icon
 const ISS_ICON = L.icon({
     iconUrl: '../../assets/img/international-space-station-icon.png', 
     iconSize:     [50, 50], // size of the icon
     iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
 });
 
-// EXO 1.4
-const MARKER = L.marker([51.5, -0.09], {icon: ISS_ICON}).addTo(map);
-
+//Add marker ISS_ICON on Map
+const MARKER = L.marker([0, 0], {icon: ISS_ICON}).addTo(MAP);
 /*******************
   SUIVI ISS
 *******************/
+async function issAPI(map,marker){
+    try{
+        //Get Lat-long ISS from API via Fetch (RESPONSE catch JSON)
+        const RESPONSE = await fetch('http://api.open-notify.org/iss-now.json');
 
-// EXO 2.1 et EXO 2.2
-function issAPI(map,MARKER) {
-    $.getJSON('http://api.open-notify.org/iss-now.json?callback=?', function(data) {
-            let lat = data['iss_position']['latitude'];
-            let lon = data['iss_position']['longitude'];
-        
-            // See leaflet docs for setting up icons and map layers
-            // The update to the map is done here:
-            MARKER.setLatLng([lat, lon]);
-            circle.setLatLng([lat, lon]);
-            map.panTo([lat, lon], animate=true); 
-        });
-    try{ 
-        division(); 
-        }catch(err){ 
-        alert(err.message); 
-        }finally{ 
-        alert(`Ce message s'affichera quoiqu'il arrive`); 
-        }     
-        setInterval(issAPI(map,MARKER), 1000); //EXO 2.3
+        //Convert JSON to object
+        const DATA = await RESPONSE.json();
+
+        //ISS Coordenate
+        const LAT = DATA.iss_position.latitude;
+        const LONG = DATA.iss_position.longitude;
+
+        // Set view os ISS coordonate
+        map.setView([LAT,LONG],5);
+        console.log(marker);
+
+        //Set coordonate marker on ISS
+        marker.setLatLng([LAT,LONG]);
+
+    }catch(error){
+        console.error(error);
+    };
 }
+
+//Syntaxe Fetch().then()
+function issAPIthen(map,marker){
+    fetch('http://api.open-notify.org/iss-now.json')
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            const LAT = DATA.iss_position.latitude;
+            const LONG = DATA.iss_position.longitude;
+
+            map.setView([LAT,LONG],5);
+
+            marker.setLatLng([LAT,LONG]);
+        })
+        .catch(error => {
+            console.error(error);
+        });
+}
+
+//Follow in quasi real-time of ISS
+setInterval(()=>{issAPI(MAP,MARKER)},1000); //EXO 2.3
 
 /***************
   METEO LOCALE
 ***************/
-// EXO 3.1
+//Creation of a paragraph
+const PARAGRAPH = document.createElement("p");
+
+//Paragraph Style
+PARAGRAPH.style.height  = "300px";
+PARAGRAPH.style.width  = "200px";
+PARAGRAPH.style.margin  = "16px auto";
+PARAGRAPH.style.border  = "3px solid grey";
+PARAGRAPH.style.padding  = "16px 12px 24px";
+
+//target .cardMeteo
+const ARTICLE = document.querySelector(".cardMeteo");
+const BUTTON = document.querySelector("button");
+//Insert PARAGRAPH Before button
+ARTICLE.insertBefore(PARAGRAPH,BUTTON);
+
+//addInfo
+function addInfo(HTMLElement, text){
+    HTMLElement.innerText = text;
+}
+
+BUTTON.addEventListener("click", async(event) => {
+    try{
+        //Resquest METEO API
+        const RESPONSE = await fetch('https://prevision-meteo.ch/services/json/toulouse');
+
+        //Convert JSON to Objetct
+        const DATA = await RESPONSE.json();
+
+        console.log(DATA);
+
+        const CURRENT_CONDITION = DATA.current_condition.condition;
+        const CURRENT_TEMP = DATA.current_condition.tmp;
+        const MAX_TEMP_DAY_0 = DATA.fcst_day_0.tmax;
+        const MIN_TEMP_DAY_0 = DATA.fcst_day_0.tmin;
+
+        const TEXT = `Condition : ${CURRENT_CONDITION} - Température actuelle : ${CURRENT_TEMP} -
+            Température Maximale : ${MAX_TEMP_DAY_0} - 
+            Température Minimale : ${MIN_TEMP_DAY_0}`
+
+        addInfo(PARAGRAPH,TEXT);
+    }catch(error){
+        console.error(error);
+    }
+})
